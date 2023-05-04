708d452fe21ffafcfe8ef13c00bc226a3055054a
Clawber <jssarenas@up.edu.ph>
2023-05-04 22:55:06 +0800

    Added leaflet boilerplate

--- svelte_crash/src/routes/+page.svelte
+++ svelte_crash/src/routes/+page.svelte
@@ -1,30 +1,7 @@
 <script>
-  let count = 0;
-
-  function increment() {
-    count += 1;
-  }
-
-  function decrement() {
-    count += -1;
-  }
+	import LeafletMap from '$lib/Map.svelte';
 </script>
 
-
-
-<h1>Number: {count} </h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-
-<button on:click={decrement}>Decrement</button>
-<button on:click={increment}>Increment</button>
-
-<a href="/about">Go to about</a>
-
-
-<style>
-  p {
-    font-size: medium;
-    color: red;
-  }
-</style>
+<main>
+	<LeafletMap />
+</main>
